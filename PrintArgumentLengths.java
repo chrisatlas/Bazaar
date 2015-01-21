adc97234a8987348450be7dee4921ad1875b1c57
Chris <vigliettachris@gmail.com>
2015-01-20 18:01:57 -0800

    Added IF statement and PRINT feature

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,4 +1,7 @@
-public class PrintArgumentLengths {
-  public static void main(String[] args) {
-        }
+public class PrintArgumentLengths { 
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Hey there, pretty lady");
+		}
+	}
 }
