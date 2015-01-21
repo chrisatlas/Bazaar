0a06975775c03a86ca54b7650d309bf2d587a187
Chris <vigliettachris@gmail.com>
2015-01-20 18:25:19 -0800

    Fixed Conditional Statement and Print Ft.

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,7 +1,8 @@
 public class PrintArgumentLengths { 
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			System.out.println("Hey there, pretty lady");
+			System.out.println("Usage: java PrintArgumentLengths <any number of arguments>");
+			return;
 		}
 	}
 }
